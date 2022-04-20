d65317d93983c725b2c5d57eb75b9e4e18d3bcea
alfredlucero <alfred.lucero@ucla.edu>
2022-04-19 23:41:53 -0700

    Added DemoArrays/DemoLists

---
+++ HelloWorld/DemoLists.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+namespace HelloWorld.DemoLists
+{
+    public class DemoLists
+    {
+        void Test()
+        {
+            // Lists - dynamic size
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Index of 1: ", numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1: ", numbers.LastIndexOf(1));
+            Console.WriteLine("Count: ", numbers.Count);
+
+            numbers.Remove(1);
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
